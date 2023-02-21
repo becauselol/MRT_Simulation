f39af4e066308d011d9eba222462881cef2ba6ed
becauselol <63877847+becauselol@users.noreply.github.com>
2023-02-21 18:45:47 +0800

    Adding JSDoc
    Documentation is important. Adding some DocStrings

--- classes/station.js
+++ classes/station.js
@@ -1,4 +1,15 @@
+/** Class representing a Station. */
 class Station {
+	/**
+     * Create a Station.
+     * @param {number} id - unique id of the station
+     * @param {number} start - the unique id of the starting station.
+     * @param {number} target - the unique id of the next station to alight at.
+     * @param {number} end - the unique id of the station wher the user journey ends
+     * @param {Array} path - the path commuters will take. It only stores the locations where users need to change lines
+     * @param {number} location - the unique id of the station/train that it is at 
+     * @param {number} state - the state of the agent
+     */
 	constructor(id, x, y, name="", codes = []) {
 		this.id = id;
 		this.x = x;
--- classes/train.js
+++ classes/train.js
@@ -1,8 +1,27 @@
+/* variable containing the various TrainStates */
+const TrainState = {
+	"WAITING": 101, // train is waiting for people to board
+	"MOVING": 102, // train is moving from one place to the next
+	"TERMINATED": 103, //train is to be removed from the system
+	"REACHED": 104, // reached the target station
+}
+
+
+/** Class representing a Train. */
 class Train {
-	constructor(id, path, curPos, state, capacity=300) {
+	/**
+     * Create a Commuter.
+     * @param {number} id - unique id of the commuter
+     * @param {Array} path - the path commuters will take. It only stores the locations where users need to change lines
+     * @param {number} location - the unique id of the station/train that it is at 
+     * @param {number} state - the state of the agent
+     */
+	constructor(id, path, currentStation, state=TrainState.WAITING, capacity=300) {
 		this.id=id;
 		this.path=path;
-		this.curPos=curPos;
+		this.curPos=0;
+		this.curStation = path[currentStation]
+		this.nextStation = path[currentStation + 1]
 		this.state=state;
 		this.capacity=capacity;
 	}
