350479d95c85d128f6b2d3d9dbfd3ebcddf5da09
kaixinchloe260 <kaixinchloe260@gmail.com>
2023-03-24 00:16:12 +0800

    Boxplot w fake data

--- classes/PlotGraph.js
+++ classes/PlotGraph.js
@@ -1,38 +1,29 @@
 
-Plotly.newPlot('chart1', [{
-  "type": "box",
-  "name": "just q1/median/q3",
-  "offsetgroup": "1",
-  "q1": [ 1, 2, 1 ],
-  "median": [ 2, 3, 2 ],
-  "q3": [ 3, 4, 3 ]
-},
-{
-  "type": "box",
-  "name": "q1/median/q3/lowerfence/upperfence",
-  "offsetgroup": "2",
-  "q1": [ 1, 2, 1 ],
-  "median": [ 2, 3, 2 ],
-  "q3": [ 3, 4, 3 ],
-  "lowerfence": [ 0, 1, 0 ],
-  "upperfence": [ 4, 5, 4 ]
-},
-{
-  "type": "box",
-  "name": "all pre-computed stats",
-  "offsetgroup": "3",
-  "q1": [ 1, 2, 1 ],
-  "median": [ 2, 3, 2 ],
-  "q3": [ 3, 4, 3 ],
-  "lowerfence": [ 0, 1, 0 ],
-  "upperfence": [ 4, 5, 4 ],
-  "mean": [ 2.2, 2.8, 2.2 ],
-  "sd": [ 0.4, 0.4, 0.4 ],
-  "notchspan": [ 0.2, 0.1, 0.2 ]
-}], {
-boxmode: 'group',
-legend: {
-x: 0,
-y: 1, yanchor: 'bottom'
+data1 = new FakeTrainData();
+data2 = new FakeLineData();
+
+var plot_data = []
+
+
+for (j = 0; j < data1.lines.length; j++){
+  line = data1.lines[j]
+  var line_data ={"type": "box", "name" : line, "q1":[data1.store[line].q25()],
+  "median": [data1.store[line].getMedian()], "q3": [data1.store[line].q75()], "lowerfence": [data1.store[line].q25() - 1.5*(data1.store[line].q75()-data1.store[line].q25())], 
+  "upperfence": [data1.store[line].q75()+ 1.5*(data1.store[line].q75()-data1.store[line].q25())], "mean":[data1.store[line].getMean()], "sd" : [data1.store[line].getStd()] };
+	    plot_data.push(line_data); 
 }
-})
+
+console.log(plot_data);
+
+var layout = {
+  boxmode: 'group',
+  legend: {x: 1,y: 0, yanchor: 'bottom'}, 
+  title : "Line Waiting Time (Boxplot)",
+  yaxis: {
+    title: 'waiting time',
+    zeroline: false
+  },
+  boxmode: 'group'
+};
+
+Plotly.newPlot('chart1', plot_data, layout)
